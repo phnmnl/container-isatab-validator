5810921bf098a3f36a4c55fe3ef3ea14d2b6d652
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-06-21 18:26:04 +0100

    Implement write log to file

--- run_validator.py
+++ run_validator.py
@@ -7,6 +7,7 @@ try:
     from isatools import isatab
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
+log_msg_stream = None
 with zipfile.ZipFile(isa_archive, 'r') as z:
     z.extractall('./')
     i_files = [f for f in z.filelist if os.path.basename(f.filename).startswith('i_')]
@@ -14,3 +15,6 @@ with zipfile.ZipFile(isa_archive, 'r') as z:
     if len(i_files) == 1:
         print("Loading: " + i_files[0].filename)
         log_msg_stream = isatab.validate2(open(i_files[0].filename))
+if log_msg_stream is not None:
+    with open('log.txt', 'w') as f:
+        f.write(log_msg_stream.getvalue())
