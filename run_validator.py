6426e8e0b323f7564e89686a8ae603fab9e6c449
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-07-20 11:39:13 +0100

    Implement tmp folder cleanup

--- run_validator.py
+++ run_validator.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zipfile
+import shutil
 
 isa_archive = sys.argv[1]
 try:
@@ -9,12 +10,13 @@ except ImportError as e:
     raise RuntimeError("Could not import isatools package")
 log_msg_stream = None
 with zipfile.ZipFile(isa_archive, 'r') as z:
-    z.extractall('./')
+    z.extractall('./tmp')
     i_files = [f for f in z.filelist if os.path.basename(f.filename).startswith('i_')]
     print(i_files)
     if len(i_files) == 1:
-        print("Loading: " + i_files[0].filename)
-        log_msg_stream = isatab.validate2(open(i_files[0].filename))
+        print("Loading: " + os.path.join('tmp', i_files[0].filename))
+        log_msg_stream = isatab.validate2(open(os.path.join('tmp', i_files[0].filename)))
 if log_msg_stream is not None:
     with open('log.txt', 'w') as f:
         f.write(log_msg_stream.getvalue())
+shutil.rmtree('./tmp')
