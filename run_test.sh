19d20ce928ebf39b7be7ceb781496f46dfec52cc
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-11-15 13:10:39 +0000

    Add test

---
+++ run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y --no-install-recommends wget zip
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/i_investigation.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/s_BII-S-1.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/s_BII-S-2.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_metabolome.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_microarray.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_proteome.txt
+zip -r BII-I-1.zip i_investigation.txt s_BII-S-1.txt s_BII-S-2.txt a_metabolome.txt a_microarray.txt a_proteome.txt
+
+python3 run_validator.py BII-I-1.zip
+
+# check that files were created
+if ! [ -e "/report.json" ]; then
+	echo "Validation report file doesn't exist"
+	exit 1
+fi
+
+echo "All files created successfully"
--- run_validator.py
+++ run_validator.py
@@ -2,21 +2,23 @@ import sys
 import os
 import zipfile
 import shutil
+import json
 
 isa_archive = sys.argv[1]
 try:
     from isatools import isatab
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
-log_msg_stream = None
+report = None
 with zipfile.ZipFile(isa_archive, 'r') as z:
     z.extractall('./tmp')
     i_files = [f for f in z.filelist if os.path.basename(f.filename).startswith('i_')]
     print(i_files)
     if len(i_files) == 1:
         print("Loading: " + os.path.join('tmp', i_files[0].filename))
-        log_msg_stream = isatab.validate2(open(os.path.join('tmp', i_files[0].filename)))
-if log_msg_stream is not None:
-    with open('log.txt', 'w') as f:
-        f.write(log_msg_stream.getvalue())
+        with open(os.path.join('tmp', i_files[0].filename)) as in_fp:
+            json_report = isatab.validate2(in_fp)
+            if json_report is not None:
+                 with open("report.json", "w") as out_fp:
+                     json.dump(json_report, out_fp)
 shutil.rmtree('./tmp')
