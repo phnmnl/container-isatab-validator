1491dbe7bb8aec18fb5365ce817553feaf022a16
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-02-15 15:43:35 +0000

    update test data location

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y --no-install-recommends wget zip
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/i_investigation.txt
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/s_BII-S-1.txt
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/s_BII-S-2.txt
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_metabolome.txt
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_microarray.txt
-wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/tab/BII-I-1/a_proteome.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/i_investigation.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/s_BII-S-1.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/s_BII-S-2.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_metabolome.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_microarray.txt
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_proteome.txt
 zip -r BII-I-1.zip i_investigation.txt s_BII-S-1.txt s_BII-S-2.txt a_metabolome.txt a_microarray.txt a_proteome.txt
 
 run_validator.py BII-I-1.zip
