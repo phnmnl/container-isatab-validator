10f5176dfcb8c04d8c5d0f59f9fb6b156f9e950e
David Johnson <djcomlab@gmail.com>
2018-01-19 15:46:40 +0000

    Debug missing output json

--- run_validator.py
+++ run_validator.py
@@ -14,12 +14,11 @@ if not os.path.exists(isa_path):
     sys.exit(0)
 report = None
 i_files = glob.glob('i_*.txt')
+print(i_files)
 if len(i_files) == 1:
     i_file_name = next(iter(i_files))
-    print('Loading: {}'.format(i_file_name))
     with open(i_file_name) as in_fp:
         json_report = isatab.validate(in_fp)
         if json_report is not None:
             with open('report.json', 'w') as out_fp:
                 json.dump(json_report, out_fp, indent=4)
-                print(json.dumps(json_report, indent=4))
